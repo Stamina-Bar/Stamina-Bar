c67cb35b847072f5c6b8a7bdd0973e7c4dd5942d
Bryce Ellis <zbellis03@gmail.com>
2022-11-18 23:01:16 -0500

    Initial Commit

---
+++ Stamina Bar Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Stamina Bar Watch App
+//
+//  Created by Bryce Ellis on 11/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stamina Bar Watch App/Stamina_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stamina_BarApp.swift
+//  Stamina Bar Watch App
+//
+//  Created by Bryce Ellis on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Stamina_Bar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
