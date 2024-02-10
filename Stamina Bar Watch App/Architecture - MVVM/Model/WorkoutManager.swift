70c698a40c60d7a253c9f5877334eb305cb1fc63
Bryce <zbellis03@gmail.com>
2024-02-10 12:06:50 -0600

    Refactor

--- Stamina Bar Watch App/Architecture - MVVM/Model/WorkoutManager.swift
+++ Stamina Bar Watch App/Architecture - MVVM/Model/WorkoutManager.swift
@@ -11,7 +11,6 @@ import HealthKit
 class WorkoutManager: NSObject, ObservableObject {
     private var healthUpdateTimer: Timer?
 
-    // Good for dynamically checking the type of workout a user is doing
     var selectedWorkout: HKWorkoutActivityType? {
         didSet {
             guard let selectedWorkout = selectedWorkout else { return }
@@ -31,34 +30,28 @@ class WorkoutManager: NSObject, ObservableObject {
     let healthStore = HKHealthStore()
     var session: HKWorkoutSession?
     
-    // Start the workout.
     func startWorkout(workoutType: HKWorkoutActivityType) {
         let configuration = HKWorkoutConfiguration()
         configuration.activityType = workoutType
         configuration.locationType = .outdoor
         
-        // Create the session and obtain the workout builder.
         do {
             session = try HKWorkoutSession(healthStore: healthStore, configuration: configuration)
             builder = session?.associatedWorkoutBuilder()
         } catch {
-            // Handle any exceptions.
             return
         }
         
-        // Setup session and builder.
         session?.delegate = self
         builder?.delegate = self
         
-        // Set the workout builder's data source.
         builder?.dataSource = HKLiveWorkoutDataSource(healthStore: healthStore,
                                                       workoutConfiguration: configuration)
         
-        // Start the workout session and begin data collection.
         let startDate = Date()
         session?.startActivity(with: startDate)
         builder?.beginCollection(withStart: startDate) { (success, error) in
-            // The workout has started.
+
         }
         
         healthUpdateTimer = Timer.scheduledTimer(withTimeInterval: 20, repeats: true) { [weak self] _ in
@@ -70,14 +63,11 @@ class WorkoutManager: NSObject, ObservableObject {
         }
     }
     
-    // Request authorization to access HealthKit.
     func requestAuthorization() {
-        // The quantity type to write to the health store.
         let typesToShare: Set = [
             HKQuantityType.workoutType()
         ]
         
-        // The quantity types to read from the health store.
         let typesToRead: Set = [
             HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!,
             HKQuantityType.quantityType(forIdentifier: .heartRateVariabilitySDNN)!,
@@ -91,15 +81,13 @@ class WorkoutManager: NSObject, ObservableObject {
             HKObjectType.activitySummaryType()
         ]
         
-        // Request authorization for those quantity types.
         healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { (success, error) in
-            // Handle error.
+
         }
     }
     
     // MARK: - Session State Control
     
-    // The app's workout state.
     @Published var running = false
     
     func togglePause() {
@@ -126,7 +114,6 @@ class WorkoutManager: NSObject, ObservableObject {
 
     }
     
-    // MARK: - Workout Metrics
     @Published var heartRateVariability: Double = 0
     @Published var averageHeartRate: Double = 0
     @Published var activeEnergy: Double = 0
@@ -182,7 +169,6 @@ extension WorkoutManager: HKWorkoutSessionDelegate {
             self.running = toState == .running
         }
         
-        // Wait for the session to transition states before ending the builder.
         if toState == .ended {
             builder?.endCollection(withEnd: date) { (success, error) in
                 self.builder?.finishWorkout { (workout, error) in
@@ -215,7 +201,6 @@ extension WorkoutManager: HKLiveWorkoutBuilderDelegate {
             
             let statistics = workoutBuilder.statistics(for: quantityType)
             
-            // Update the published values on the main thread.
             DispatchQueue.main.async {
                 self.updateForStatistics(statistics)
             }
@@ -223,38 +208,25 @@ extension WorkoutManager: HKLiveWorkoutBuilderDelegate {
     }
     
     func fetchMostRecentHRV() {
-        // Define the Heart Rate Variability type using HealthKit's standard nomenclature.
         guard let hrvType = HKQuantityType.quantityType(forIdentifier: .heartRateVariabilitySDNN) else {
             print("HRV type is not available.")
             return
         }
-        // Create a predicate to fetch samples from the distant past to the current date.
-        // This ensures we get the most recent sample.
         let mostRecentPredicate = HKQuery.predicateForSamples(withStart: Date.distantPast, end: Date(), options: .strictEndDate)
         
-        // Define a sort descriptor to sort the samples by start date in descending order.
-        // This ensures the most recent sample is first.
         let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
         
-        // Create a query to fetch Heart Rate Variability samples.
-        // Limit the query to return only the most recent sample (limit: 1).
         let query = HKSampleQuery(sampleType: hrvType, predicate: mostRecentPredicate, limit: 1, sortDescriptors: [sortDescriptor]) { [weak self] (_, samples, _) in
-            // Process the query results on the main thread to update the UI.
             DispatchQueue.main.async {
-                // Ensure the samples are of the correct type (HKQuantitySample) and get the first one.
                 guard let samples = samples as? [HKQuantitySample], let mostRecentSample = samples.first else {
                     return
                 }
-                // Update the heartRateVariability property with the latest HRV value.
-                // Convert the HRV value to a double value in milliseconds (ms).
                 self?.heartRateVariability = mostRecentSample.quantity.doubleValue(for: HKUnit(from: "ms"))
             }
         }
-        // Execute the query on the health store.
         self.healthStore.execute(query)
     }
     
-    // Execute query to retrieve basal (resting) energy
     func fetchDailyBasalEnergyBurn() {
         guard let restingEnergyType = HKObjectType.quantityType(forIdentifier: .basalEnergyBurned) else {
             print("Basal energy burn type is not available")
@@ -281,8 +253,6 @@ extension WorkoutManager: HKLiveWorkoutBuilderDelegate {
         healthStore.execute(query)
     }
 
-    
-    // Execute query to retrieve active energy
     func fetchDailyActiveEnergyBurned() {
         guard let activeEnergyType = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned) else {
             print("Daily active energy burn type is not available")
@@ -309,8 +279,6 @@ extension WorkoutManager: HKLiveWorkoutBuilderDelegate {
         healthStore.execute(query)
     }
 
-    
-    // Fetch the current day's step count
     func fetchDailyStepCount() {
         guard let stepCountType = HKObjectType.quantityType(forIdentifier: .stepCount) else {
             print("Step Count type is not available.")
@@ -321,9 +289,7 @@ extension WorkoutManager: HKLiveWorkoutBuilderDelegate {
         let now = Date()
         let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
         
-        // Start timing
         let startTime = Date()
-        
         
         let query = HKStatisticsQuery(quantityType: stepCountType, quantitySamplePredicate: predicate, options: .cumulativeSum) { _, result, error in
             // Calculate elapsed time
@@ -335,7 +301,6 @@ extension WorkoutManager: HKLiveWorkoutBuilderDelegate {
                 return
             }
             
-            // Update the step count
             DispatchQueue.main.async {
                 self.dailyStepCount = Int(sum.doubleValue(for: HKUnit.count()))
                 print("Step Count: \(self.dailyStepCount) steps")
@@ -343,39 +308,29 @@ extension WorkoutManager: HKLiveWorkoutBuilderDelegate {
             }
         }
         
-        // Execute the query
         healthStore.execute(query)
     }
-
     
-    // Fetch the most recent VO2 Max value from HealthKit
     func fetchMostRecentVO2Max() {
-        // Define the VO2 Max data type
         guard let vo2MaxType = HKObjectType.quantityType(forIdentifier: .vo2Max) else {
             print("VO2 Max type is not available.")
             return
         }
         
-        // Create a sort descriptor to fetch the latest sample
         let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
         
-        // Build a query to fetch the most recent VO2 Max sample
         let query = HKSampleQuery(sampleType: vo2MaxType, predicate: nil, limit: 1, sortDescriptors: [sortDescriptor]) { _, results, error in
             guard let vo2MaxSample = results?.first as? HKQuantitySample else {
                 print("Failed to retrieve VO2 Max data: \(error?.localizedDescription ?? "Unknown error")")
                 return
             }
             
-            // Update VO2 Max values
             DispatchQueue.main.async {
                 self.previousVO2Max = self.currentVO2Max
                 self.currentVO2Max = vo2MaxSample.quantity.doubleValue(for: HKUnit(from: "ml/kg*min"))
             }
         }
-        
-        // Execute the query
+
         healthStore.execute(query)
     }
-    
-    
 }
