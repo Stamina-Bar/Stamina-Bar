4b2bf1c9eba02ec16cbaffece6a34797292030bb
Bryce <zbellis03@gmail.com>
2024-03-14 10:49:14 -0400

    merging this as new main

--- Stamina Bar Watch App/Architecture - MVVM/View/ControlsView.swift
+++ Stamina Bar Watch App/Architecture - MVVM/View/ControlsView.swift
@@ -12,31 +12,35 @@ struct ControlsView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
     
     var body: some View {
-        HStack {
-            VStack {
-                Button {
-                    workoutManager.endWorkout()
-                    HapticManager.stopHaptic()
-                    
-                } label: {
-                    Image(systemName: "xmark")
+        VStack {
+            Text("On the Move?")
+            Spacer()
+            HStack {
+                VStack {
+                    Button {
+                        workoutManager.endWorkout()
+                        HapticManager.stopHaptic()
+                        
+                    } label: {
+                        Image(systemName: "xmark")
+                    }
+                    .tint(.red)
+                    .font(.title2)
+                    Text("End")
                 }
-                .tint(.red)
-                .font(.title2)
-                Text("End")
-            }
-            VStack {
-                Button {
-                    workoutManager.togglePause()
-                    
-                    workoutManager.running ? HapticManager.stopHaptic() : HapticManager.startHaptic()
-                    
-                } label: {
-                    Image(systemName: workoutManager.running ? "pause" : "play")
+                VStack {
+                    Button {
+                        workoutManager.togglePause()
+                        
+                        workoutManager.running ? HapticManager.stopHaptic() : HapticManager.startHaptic()
+                        
+                    } label: {
+                        Image(systemName: workoutManager.running ? "pause" : "play")
+                    }
+                    .tint(.yellow)
+                    .font(.title2)
+                    Text(workoutManager.running ? "Pause" : "Resume")
                 }
-                .tint(.yellow)
-                .font(.title2)
-                Text(workoutManager.running ? "Pause" : "Resume")
             }
         }
     }
