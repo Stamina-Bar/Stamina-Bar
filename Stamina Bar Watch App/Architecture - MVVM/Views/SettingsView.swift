e67dbbccce6e6669be5f9af0c0b44fb9a9b37cf0
Bryce <zbellis03@gmail.com>
2024-04-16 19:49:24 -0500

    beautiful changes

--- Stamina Bar Watch App/Architecture - MVVM/Views/SettingsView.swift
+++ Stamina Bar Watch App/Architecture - MVVM/Views/SettingsView.swift
@@ -9,41 +9,96 @@ import SwiftUI
 import Foundation
 
 struct SettingsView: View {
+    @Environment(\.dismiss) var dismiss
     @Environment(\.presentationMode) var presentationMode
     
     // Example settings
     @AppStorage("notificationsEnabled") var notificationsEnabled: Bool = false
-    @AppStorage("theme") var theme: String = "System"
     
     // Accessing app version and build number
     let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Unknown"
     let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "Unknown"
     
     var body: some View {
-        NavigationView {
+        TabView {
             ScrollView {
                 Section {
-                    VStack(alignment: .center, spacing: 10) {
-                        Text("Scan here for FAQ's")
-                            .font(.headline)
-                            .foregroundColor(.primary)
-                            .multilineTextAlignment(.center)
-                        
-                        Image("QR-Code") // Make sure this is the name of your QR code image asset
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
+                    if #available(watchOS 10.0, *) {
+                        ScanQRView()
+                            .containerBackground(.blue.gradient, for: .tabView)
                     }
-                    .frame(maxWidth: .infinity)
+                    else {
+                        ScanQRView()
+                    }
+                    
+                    Divider()
+                    if #available(watchOS 10.0, *) {
+                        AppVersionView()
+                            .containerBackground(.blue.gradient, for: .tabView)
+                    } else {
+                        AppVersionView()
+                    }
+                    
                 }
                 
-                Spacer()
-                Section {
-                    HStack {
-                        Text("v\(appVersion)")
-                            .foregroundColor(.gray)
-                    }
+                Button("Cancel") {
+                    dismiss()
                 }
+                
+            }
+            
+            
+            
+        }
+        
+    }
+}
+
+
+
+struct AppVersionView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    // Accessing app version and build number
+    let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Unknown"
+    let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "Unknown"
+    
+    
+    var body: some View {
+            Section {
+                VStack(alignment: .center, spacing: 10) {
+                    Text("App Version")
+                        .font(.headline)
+                    Text("v\(appVersion)")
+                        .foregroundColor(.gray)
+                        .font(.subheadline)
+                }
+            
+        }
+    }
+    
+    func dismiss2() {
+        presentationMode.wrappedValue.dismiss()
+    }
+}
+
+struct ScanQRView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ScrollView {
+            Section {
+                VStack(alignment: .center, spacing: 10) {
+                    Image("QR-Code") // Make sure this is the name of your QR code image asset
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                    Text("Scan for FAQ's")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        .multilineTextAlignment(.center)
+                }
+                .frame(maxWidth: .infinity)
             }
         }
     }
