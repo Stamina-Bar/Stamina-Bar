94a1222d4015ea66ace6444163b623cb85e666cb
Bryce <zbellis03@gmail.com>
2024-11-15 06:27:13 -0600

    greeat changes so far

---
+++ Stamina Bar Watch App/Architecture - MVVM/Views/HealthKitPage.swift
@@ -0,0 +1,18 @@
+//
+//  HealthKitPage.swift
+//  Stamina Bar Watch App
+//
+//  Created by Bryce Ellis on 11/15/24.
+//
+
+import SwiftUI
+
+struct HealthKitPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthKitPage()
+}
